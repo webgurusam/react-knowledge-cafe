11483720de7e11ebb039e6fd7a8c59dfdc8a0d9c
webgurusam <shyamghosh666@gmail.com>
2024-05-19 07:38:44 +0600

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-center">
+          React Knowledge cafe
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
